3bf0d6e4f0277cbeaf901d3c260a1fd00caff4b0
shirozo <caranzoneilbryan@gmail.com>
2025-06-13 00:44:01 +0800

    feat: Display monthly summary using ListView
    This commit implements the display of monthly financial summaries in `MonthlyActivity.java` using a `ListView`.
    
    Key changes:
    - Added a `ListView` with ID `daily_summary` to the layout (assumed, as layout file is not in diff).
    - Created an `ArrayList` of `Money` objects to hold monthly data.
    - Initialized the list with a sample `Money` object.
    - Instantiated a `ListViewAdapter` to bind the `Money` data to the `ListView`.
    - Set the adapter to the `ListView` to display the data.

--- app/src/main/java/com/shirozo/simplemoneymanagement/MonthlyActivity.java
+++ app/src/main/java/com/shirozo/simplemoneymanagement/MonthlyActivity.java
@@ -1,12 +1,17 @@
 package com.shirozo.simplemoneymanagement;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.shirozo.simplemoneymanagement.classes.Money;
+
+import java.util.ArrayList;
 
 public class MonthlyActivity extends AppCompatActivity {
 
@@ -20,5 +25,13 @@ public class MonthlyActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ArrayList<Money> monthlies = new ArrayList<>();
+        Money money = new Money(1, (float) 100, (float) 150, "December 21");
+        monthlies.add(money);
+
+        ListView listView = findViewById(R.id.daily_summary);
+        ListViewAdapter adapter = new ListViewAdapter(this, monthlies);
+        listView.setAdapter(adapter);
     }
 }
