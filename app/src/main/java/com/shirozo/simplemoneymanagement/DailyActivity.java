d27ef844a00679247d411ed1066b690238b11528
shirozo <caranzoneilbryan@gmail.com>
2025-06-14 23:15:18 +0800

    feat: Implement DailyActivity with custom adapter
    This commit introduces the `DailyActivity` which displays a list of transactions using a custom `DailyAdapter`.
    
    Key changes in `DailyActivity.java`:
    - Initialized an `ArrayList` of `Transaction` objects with sample data.
    - Retrieved the `ListView` with ID `daily_summary`.
    - Removed the default divider and set a custom divider height of 10dp for the `ListView`.
    - Created an instance of `DailyAdapter` with the current context and the list of transactions.
    - Set the `DailyAdapter` to the `ListView` to populate it with transaction data.

--- app/src/main/java/com/shirozo/simplemoneymanagement/DailyActivity.java
+++ app/src/main/java/com/shirozo/simplemoneymanagement/DailyActivity.java
@@ -1,12 +1,18 @@
 package com.shirozo.simplemoneymanagement;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.shirozo.simplemoneymanagement.adapter.DailyAdapter;
+import com.shirozo.simplemoneymanagement.classes.Transaction;
+
+import java.util.ArrayList;
 
 public class DailyActivity extends AppCompatActivity {
 
@@ -20,5 +26,19 @@ public class DailyActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        Transaction transaction = new Transaction(1, "Salary", "2025-06-14 23:45:00", "1000", 0);
+        Transaction transaction1 = new Transaction(2, "Food", "2025-06-14 23:46:00", "400", 1);
+
+        transactions.add(transaction);
+        transactions.add(transaction1);
+
+        ListView listView = findViewById(R.id.daily_summary);
+        listView.setDivider(null);
+        listView.setDividerHeight(10);
+        DailyAdapter adapter = new DailyAdapter(this, transactions);
+        listView.setAdapter(adapter);
     }
 }
